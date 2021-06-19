3e165c3c2b49b4692dcea415ffa42742066163ae
Sergii <74363342+akoross@users.noreply.github.com>
2021-06-19 13:38:57 +0300

    fixed bag run brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import cli from '../src/cli.js';
 
-const name = cli();
+console.log('\nWelcom to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 let counter = 3;
